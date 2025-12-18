be1d705eb270e936d96650a8e39d99a7d5ee9843
Mara Kim <mara@astralarium.net>
2025-12-18 00:28:41 -0800

    setup app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
+import { PixiThreeCanvas } from "./components/pixi-three-canvas";
+import { SpinnyCube } from "./components/spinny-cube";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Pixi + Three</h1>
+      <h1 className="text-lg">Pixi + Three</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <PixiThreeCanvas className="w-[80lvw] h-[80lvh]">
+          <SpinnyCube />
+        </PixiThreeCanvas>
       </div>
     </>
   );
