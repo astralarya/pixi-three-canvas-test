89cf5d9890ff54558fdd9e74e22e611c56936643
Mara Kim <mara@astralarium.net>
2025-12-18 02:06:39 -0800

    add conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,25 @@ import { PixiThreeCanvas } from "./components/pixi-three-canvas";
 import { SpinnyCube } from "./components/spinny-cube";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(true);
 
   return (
     <>
-      <h1 className="text-lg">Pixi + Three</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1 className="text-2xl">Pixi + Three</h1>
+      <div className="flex items-center gap-4">
+        <button
+          className="bg-neutral-500 p-2 rounded-sm"
+          onClick={() => setToggle((x) => !x)}
+        >
+          Toggle canvas
         </button>
+        <span>Canvas is {toggle ? "mounted" : "unmounted"}</span>
+      </div>
+      {toggle && (
         <PixiThreeCanvas className="w-[80lvw] h-[80lvh]">
           <SpinnyCube />
         </PixiThreeCanvas>
-      </div>
+      )}
     </>
   );
 }
